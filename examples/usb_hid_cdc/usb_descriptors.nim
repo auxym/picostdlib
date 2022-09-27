9cccc6ac16d4165f41066c410624d7f0341d18fe
Francis Therien <frtherien@gmail.com>
2022-09-26 21:51:46 -0400

    Implement all descriptors (except string) in Nim

--- examples/usb_hid_cdc/usb_descriptors.nim
+++ examples/usb_hid_cdc/usb_descriptors.nim
@@ -13,30 +13,100 @@ static:
     mcu=TinyUsbMcu.RP2040,
     os=TinyUsbOs.None,
     debug=false,
-    classes={UsbClass.Hid, UsbClass.Cdc},
+    cdc=1, hid=1,
     ep0size=Ep0Size,
   )
 
 
 # Device descriptor
-setDeviceDescriptor:
-  DeviceDescriptor(
-    length: sizeof(DeviceDescriptor).uint8,
-    descriptorType: UsbDescriptorType.Device,
-    usbVersion: initBcdVersion(2, 0, 0),
+setDeviceDescriptor initDeviceDescriptor(
+  usbVersion=initBcdVersion(2, 0, 0),
 
-    # These class/subclass/protocol values required for CDC
-    # See TinyUSB examples for more info.
-    class: UsbClass.Misc,
-    subclass: MiscSubclassCommon,
-    protocol: MiscProtocolIad,
+  # These class/subclass/protocol values required for CDC
+  # See TinyUSB examples for more info.
+  class=UsbClass.Misc,
+  subclass=MiscSubclassCommon,
+  protocol=MiscProtocolIad,
 
-    ep0MaxPacketSize: Ep0Size,
-    vendorId: 0xCAFE,
-    productId: 0x4005,
-    deviceVersion: initBcdVersion(0, 1, 0),
-    manufacturerStr: 1.StringIndex,
-    productStr: 2.StringIndex,
-    serialNumberStr: 3.StringIndex,
-    numConfigurations: 1
+  ep0Size=Ep0Size,
+  vendorId=0xCAFE,
+  productId=0x4005,
+  deviceVersion=initBcdVersion(0, 1, 0),
+  manufacturerStr=1.StringIndex,
+  productStr=2.StringIndex,
+  serialNumberStr=3.StringIndex,
+  numConfigurations=1
+)
+
+# HID Report Descriptor
+const
+  KeyboardReportId = 1
+  MouseReportId = 2
+  GamepadReportId = 3
+
+  myHidReportDescriptor =
+    keyboardReportDescriptor(id=KeyboardReportId) &
+    mouseReportDescriptor(id=MouseReportId) &
+    gamepadReportDescriptor(id=GamepadReportId)
+
+  hidReportDescLen = len(myHidReportDescriptor)
+
+hidReportDescriptorCallback(inst):
+  let desc {.global.} = toArrayLit myHidReportDescriptor
+  return desc[0].unsafeAddr
+
+# Configuration descriptor
+
+const
+  CdcInterface* = 0.InterfaceNumber
+  CdcDataInterface* = 1.InterfaceNumber
+  HidInterface* = 2.InterfaceNumber
+  NumInterfaces* = 3
+
+const
+  fullConfigSize = sizeof(ConfigurationDescriptor) +
+                   sizeof(CompleteCdcSerialPortInterface) +
+                   sizeof(CompleteHidInterface)
+
+  configDescriptor = initConfigurationDescriptor(
+    val=1,
+    totalLength=fullConfigSize.uint16,
+    numItf=NumInterfaces,
+    powerma=100,
   )
+
+  cdcDesc = initCompleteCdcSerialPortInterface(
+    controlItf=CdcInterface,
+    controlEpNum=1,
+    controlEpSize=8,
+    dataEpNum=2,
+    dataEpSize=64,
+    str=StringIndexNone
+  )
+
+  hidDesc = initCompleteHidInterface(
+    HidInterface,
+    reportDescLen=hidReportDescLen,
+    epIn=3,
+    epInSize=16,
+    epInterval=5,
+    str=StringIndexNone
+  )
+
+configurationDescriptorCallback(index):
+  # Can handle multiple different configurations
+  if index != configDescriptor.value: return nil
+
+  # Per USB spec, a request for the configuration descriptor must
+  # return the configuration descriptor and all associated interface 
+  # and endpoint descriptors. Here we concatenate all the descriptors.
+  const
+    fullCfg =
+      configDescriptor.serialize &
+      cdcDesc.serialize &
+      hidDesc.serialize
+  
+  # Use the toArrayLit macro to generate a static byte array and assign
+  # to a {.global.} variable to ensure that the pointer is always valid.
+  let fullCfgBytes {.global.} = toArrayLit(fullCfg)
+  return fullCfgBytes[0].addr
